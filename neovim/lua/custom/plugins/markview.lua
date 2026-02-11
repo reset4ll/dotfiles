37fc549d8397f2094aa453bb6e9e25df516f1059
reset4ll <reset4ll@users.noreply.github.com>
2026-02-11 18:05:39 +0100

    Add new plugins

---
+++ neovim/lua/custom/plugins/markview.lua
@@ -0,0 +1,10 @@
+-- For `plugins/markview.lua` users.
+
+return {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- Completion for `blink.cmp`
+    -- dependencies = { "saghen/blink.cmp" },
+}
+
