e94819a9127244188c8bd5676ead2b29b47d2c47
reset4ll <reset4ll@users.noreply.github.com>
2026-02-17 11:57:35 +0100

    update files

---
+++ neovim/lua/custom/plugins/nord-theme.lua
@@ -0,0 +1,12 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+-- example lazy.nvim install setup
+return {
+
+  "shaunsingh/nord.nvim",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+
+}
