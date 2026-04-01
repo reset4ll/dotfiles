30f1996f21e3ab2f87f06c7c972b9fbc12cc6d36
reset4ll <reset4ll@users.noreply.github.com>
2026-04-01 20:10:47 +0200

    add jupyter notebook support: <mié 2026-04-01>

--- doom/init.el
+++ doom/init.el
@@ -96,7 +96,7 @@
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
-       ;;ein               ; tame Jupyter notebooks with emacs
+       ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
        ;;llm               ; when I said you needed friends, I didn't mean...
